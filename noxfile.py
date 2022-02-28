c3cb6781a2b4937a49ac53c6bc2f5546f0fc72a2
jackwotherspoon <jackwoth@google.com>
2022-02-28 15:18:50 +0000

    remove python 3.10

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
+@nox.session(python=["3.7", "3.8", "3.9"])
 def test(session):
     session.install("-r", "requirements.txt")
     # Run py.test against the unit tests.
